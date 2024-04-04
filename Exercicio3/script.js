f6b2d24852947bc76088d9470f7b5a165bbc6fe8
isammt <bella.trigo14@gmail.com>
2024-04-04 01:30:45 -0300

    exercicio 3 de js

---
+++ Exercicio3/script.js
@@ -0,0 +1,9 @@
+var numero1 = parseFloat(prompt("Digite o primeiro número:"));
+var numero2 = parseFloat(prompt("Digite o segundo número:"));
+var numero3 = parseFloat(prompt("Digite o terceiro número:"));
+
+var soma = numero1 + numero2 + numero3;
+
+var media = soma / 3;
+
+console.log("A Media desse numeros são:" + media);
