aa30d20c316c7e70169877284d45499d43bae2d0
isammt <bella.trigo14@gmail.com>
2024-04-04 02:33:55 -0300

    exercicio 2.1 de js

---
+++ Exercicio2.1/script.js
@@ -0,0 +1,11 @@
+var numero1 = parseFloat(prompt("Digite o primeiro número:"));
+var numero2 = parseFloat(prompt("Digite o segundo número:"));
+
+var soma = numero1 + numero2;
+
+if(soma >= 100){
+    console.log("Sua variável é " + soma +" maior ou igual a 100.");
+}
+else{
+    console.log("Sua variável é menor a 100.");
+} 
