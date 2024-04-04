56b9ae6f5692fef7308a97ac190a8e3c8a86fbd7
isammt <bella.trigo14@gmail.com>
2024-04-04 00:58:39 -0300

    exercicio 2 de js

---
+++ Exercicio2/script.js
@@ -0,0 +1,5 @@
+var numero1 = prompt("Digite o primeiro número:");
+var numero2 = prompt("Digite o segundo número:");
+var numero3 = prompt("Digite o terceiro número:");
+
+console.log("Os números digitados são:" + numero1 + " " + numero2 + " " + numero3);
