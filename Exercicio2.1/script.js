bf86973853932f7f2450dfd5c6f3082522955a00
isammt <bella.trigo14@gmail.com>
2024-04-04 02:45:24 -0300

    troquei a palavra do console.log de variavel para número

--- Exercicio2.1/script.js
+++ Exercicio2.1/script.js
@@ -4,8 +4,8 @@ var numero2 = parseFloat(prompt("Digite o segundo número:"));
 var soma = numero1 + numero2;
 
 if(soma >= 100){
-    console.log("Sua variável é " + soma +" maior ou igual a 100.");
+    console.log("Seu número é " + soma +" maior ou igual a 100.");
 }
 else{
-    console.log("Sua variável é menor a 100.");
+    console.log("Sua número é menor a 100.");
 } 
