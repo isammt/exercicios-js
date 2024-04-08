9b55a0ebb1331c2c08da70df34b65c55dd08929e
isammt <bella.trigo14@gmail.com>
2024-04-07 23:50:30 -0300

    exercicio 2.3 de js

---
+++ Exercicio2.3/script.js
@@ -0,0 +1,15 @@
+function limitaPosicao(posicao) {
+  if (limitaPosicao < 0) {
+    return 0;
+  } else if (posicao > 100) {
+    return 100;
+  } else {
+    return posicao;
+  }
+}
+
+//var posicaoInimigo = 100 ;
+//var posicaoInimigo = -200 ;
+var posicaoInimigo = 10 ;
+var posicaoCorrigida = limitaPosicao(posicaoInimigo)
+console.log("a posição corrigida do inimigo é:" + posicaoCorrigida);
