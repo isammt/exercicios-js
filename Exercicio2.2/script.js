f4cd8dfa5224e87dbdf54582161de4f06c92474e
isammt <bella.trigo14@gmail.com>
2024-04-07 23:34:06 -0300

    exercicio 2.2 de js

---
+++ Exercicio2.2/script.js
@@ -0,0 +1,14 @@
+function personagemMorreu(dano, saude) {
+  if (personagemMorreu <= 0) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+var dano = 50; 
+var saude = 30;
+var resultado = personagemMorreu(dano, saude);
+console.log ("O personagem morreu?" + resultado);
+
+
